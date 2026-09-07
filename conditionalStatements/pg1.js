13de6e7e609184022683cb7b5347af2c332f4a8b
Abin-ms <abinn.mss@gmail.com>
2026-09-07 12:32:04 +0530

    new tutorial / practice code is added

---
+++ conditionalStatements/pg1.js
@@ -0,0 +1,38 @@
+// if(true){
+//     console.log(" Malabar Biriyani ");
+// }
+
+// if(false){
+//     console.log(" mutton biriyani ")
+// }
+
+// let totalPrice = 1500;
+// let totalPrice2 = 800;
+
+// if( totalPrice > 1000){
+//     console.log(" you are eligible for free Delivery....")
+// }
+
+
+// let batteryPercent = 15;
+// if( batteryPercent < 20){
+//     console.log("Low battery!please charge your phone");
+// }
+
+
+
+let stock = 5;
+if( stock > 0 ){
+    console.log("Out of dtock");
+}
+else{
+    console.log("out of stock");
+}
+
+let password = 1234;
+if( password == "1234"){
+    console.log("Login Successfull");
+}
+else{
+    console.log("Login Failed");
+}
