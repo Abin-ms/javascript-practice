bd13c3a07e417d0813b99aa58a3ab835469d0f44
Abin-ms <abinn.mss@gmail.com>
2026-09-10 12:49:25 +0530

    practice code of looping statements

---
+++ loopingstatements/pg11.js
@@ -0,0 +1,44 @@
+// let i = 1
+// for(; i<=5;i++){
+//     console.log(i)
+// }
+
+// for( i = 1 ; i <= 5;){
+//     console.log(i);
+//     i++;
+// }
+
+// for( i = 1;i <= 5 ;){
+//     i++;
+//     console.log(i);
+// }
+
+
+// let actualPassword = "1235";
+// let userPassword = prompt("Enter Your password")
+
+// while( userPassword !== actualPassword){
+//     userPassword = prompt("Enter You Password")
+// }
+
+// alert("Login succesfull!!!1")
+
+
+// let i = 1 ;
+// do{
+// console.log("do while implementation");
+// i++
+// }while( i <= 5)
+
+
+for( let i = 1 ; i<= 5;i++){
+
+    if( i % 2 == 0){
+        console.log( i+ "is even.")
+    }
+    else{
+        console.log(i+"is odd")
+    }
+    
+}
+
