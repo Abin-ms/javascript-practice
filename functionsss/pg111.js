a5d71d81e775fa7b5bae969867aaaa24ac2b2b45
Abin-ms <abinn.mss@gmail.com>
2026-09-11 15:06:19 +0530

    functions tutorial

---
+++ functionsss/pg111.js
@@ -0,0 +1,30 @@
+function greeting(){
+    console.log("hellooooo");
+}
+greeting();
+
+function coffeaMachine(customer , cofeeType , price){
+    console.log("Type of coffee : ",cofeeType);
+    // console.log(HotelTaj);
+    console.log("Boil Water");
+    console.log("Add milk");
+    console.log("Add Coffe powder");
+    console.log("Add Sugar.");
+    console.log(" Pour coffe in a cup.");
+    console.log("Total Price is : ",price);
+    console.log( "Prepared Coffe for : ",customer);
+}
+coffeaMachine( "Rose","Cold Coffee",65)
+
+
+function Message(company)
+{
+    for(let i = 1 ; i <= 50 ; i++){
+        console.log(i);
+        console.log("join the great company : ",company);
+        console.log("Feel the actual Heaven");
+
+    }
+}
+
+Message("TestYantra")
